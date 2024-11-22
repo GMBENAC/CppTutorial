9499603a931bd0aacf456935e02bc05f31995ef2
GMBENAC <benacmgabriel@gmail.com>
2024-11-22 12:03:08 -0300

    Update and rename 3_type_casting .py to 106_ Arithmeticoperators.cpp

---
+++ 106_ Arithmeticoperators.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+
+int main()
+{
+    // aritimetic operators = return the result of a specific
+    //                        aritimetic operation (+ - * /)
+
+    int students = 20;
+
+    students = students + 1;
+
+    std::cout << students;
+
+    return 0;
+}
