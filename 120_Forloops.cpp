3cd76ba7dae4dcc95e6038d724c8e32111ab0a1c
GMBENAC <benacmgabriel@gmail.com>
2025-02-24 11:20:56 -0300

    Add files via upload

---
+++ 120_Forloops.cpp
@@ -0,0 +1,37 @@
+# include <iostream>
+
+int main()
+{
+   for(int i = 1; i <= 5; i++){
+    std::cout << "HAPPY NEW YEAR!\n";
+   }  
+
+     for(int i = 1; i <= 10; i++){
+    std::cout << i << '\n';
+   }  
+
+  std::cout << "HAPPY NEW YEAR!\n";
+
+     for(int i = 0; i <= 10; i+=2){
+    std::cout << i << '\n';
+   }  
+
+  std::cout << "HAPPY NEW YEAR!\n";
+
+  for(int i = 10; i >= 0; i--){
+    std::cout << i << '\n';
+   }  
+
+  std::cout << "HAPPY NEW YEAR!\n";
+
+  for(int i = 10; i >= 0; i-=2){
+    std::cout << i << '\n';
+   }  
+
+  std::cout << "HAPPY NEW YEAR!\n";
+
+
+
+
+return 0;
+}
